667db554fe0e9466c5102d539dc9856db3fb3767
chan <baewonchan1@naver.com>
2020-07-26 20:42:17 +0900

    Add fetch and print weather data

--- src/components/Weather/index.js
+++ src/components/Weather/index.js
@@ -1,16 +1,52 @@
 import React from "react";
-
+const API_WEATHERS = "http://localhost:8888/weather-service/weathers?";
 // Stateless component
-const Weather = (props) => {
-  const { cityName } = props.match.params;
-  console.log(cityName);
+//const Weather = (props) => {
+  // 1 .
+  // class 형태로 변경 후 fetch 선택 도시의 날씨
+  // 2 .
+  // react Hook << 검색
+  // state, setState
+class Weather extends React.Component {
+    state = {
+            weatherData : {
+                weather : [
+                    { main : null,description : null }
+                ],
+                main : {
+                    temp : 0,
+                }
+            },
+        };
 
-  return (
-    <div>
-      <h1>Weather : {cityName}</h1>
-      <p>Here is front page.!</p>
-    </div>
-  );
+
+    componentDidMount() {
+        const { cityName } = this.props.match.params;
+
+        const weathersData = fetch(API_WEATHERS +'cityName='+ cityName)
+              .then((res) => res.json())
+              .then((data) => {
+                this.setState({
+                  weatherData : data,
+                });
+              });
+
+    }
+
+     render() {
+         const { weatherData } = this.state;
+
+         const main = weatherData.weather[0].main
+         const description = weatherData.weather[0].description
+         const celsius = (weatherData.main.temp - 273.15).toFixed(2); // kelvin to celsius
+
+        return (
+            <div>
+              <p>{main}</p>
+              <p>{description}</p>
+              <p>{celsius}</p>
+            </div>
+          );
+     }
 };
-
 export default Weather;
